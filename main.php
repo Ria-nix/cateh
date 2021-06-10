366bbf46eb2f065894791ec593c034c9ed9356b9
Ria-nix <podkovadasha@gmail.com>
2021-06-10 16:33:08 +0200

    nav

--- main.php
+++ main.php
@@ -10,44 +10,50 @@
     <title>Главная: Системные администраторы</title>
 </head>
 <body>
-    <nav>        
+    <!-- HEADER -->
+    <nav>   
+        <!-- NAVIGATION PANEL      -->
         <div class="navigation">
             <div class="logo_wrap" onselectstart="return false" onmousedown="return false">
                 <img src="icon/logo.png" alt="">
             </div>
             <div class="panel_buttons">
-                <span class="button border sysadmin">
+                <span class="button border main">
                     <img src="icon/Untitled.svg" alt="alt">
-                    <p class="font_16">Системные администраторы</p>
+                    <p class="font_18">Системные администраторы</p>
                 </span>
                 <span class="button border plus">
                     <img src="icon/plus-solid.svg" alt="alt">
                 </span>
-                <span class="button border">
-                    <img src="icon/build.svg" class="main" alt="alt">
-                    <p class="font_16">Организации</p>
+                <span class="button border main">
+                    <img src="icon/build.svg" alt="alt">
+                    <p class="font_18">Организации</p>
                 </span>
                 <span class="button border plus">
                     <img src="icon/plus-solid.svg" alt="alt">
                 </span>
-                <span class="button border">
-                    <img src="icon/settings.svg" class="settings" alt="alt">
-                    <p class="font_16"></p>
+                <span class="button border settings">
+                    <img src="icon/settings.svg" alt="alt">
+                    <p class="font_18"></p>
                 </span>
-                <span class="button border">                    
-                    <p class="font_16">Выйти</p>
-                    <img src="icon/exitdoor_87195.svg" class="door" alt="alt">
+                <span class="button border door">                    
+                    <p class="font_18">Выйти</p>
+                    <img src="icon/exitdoor_87195.svg" alt="alt">
                 </span>
             </div>
         </div>
         <hr>
-        <div class="trow_up">
-            <br>
-            <h1>HI</h1>
-            <br>
+        <!-- THE END OF NAVIGATION PANEL -->
+        
+        <div class="trow_up">            
+            <span class="button border trow_item">
+                <img src="icon/close.svg" alt="alt">
+                <p class="font_18">Сбросить</p>
+            </span>
         </div>
     </nav>
 
+    <!-- CONTENT -->
     <div class="sidebar_wrap">
         <div class="head_table">
         </div>
