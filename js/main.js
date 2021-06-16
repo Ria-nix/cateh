53e6938b05343035cd6c727388b4d01aee20e7bd
Ria-nix <podkovadasha@gmail.com>
2021-06-16 13:27:02 +0200

    SCROLL

--- js/main.js
+++ js/main.js
@@ -4,12 +4,13 @@ $('.toggle_but').click(function(){
     if(!button){
         $('.toggle_menu_fon').removeClass('none').addClass('grid');
         $('.toggle_but').addClass('close');
+        button = true;
         console.log('click')
     }
     else{
-        $('.toggle_menu_fon').addClass('none').remove('grid');
+        $('.toggle_menu_fon').removeClass('grid').addClass('none');
         $('.toggle_but').removeClass('close');
+        button = false;
         console.log('click2')
-    }
-    
+    }    
 });
