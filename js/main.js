1a4b8b9a9b21f43995d2fb8236a3645a22b924fb
Ria-nix <podkovadasha@gmail.com>
2021-06-16 11:37:08 +0200

    jepa

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-$('.toggle_but').click(function(){
-    console.log('click');
-})
+// $('.toggle_but').click(function(){
+//     if( $('.toggle_menu_fon').hasClass() == 'none'){
+//         $('.toggle_menu_fon').removeClass('none');
+//         $('.toggle_but').addClass('close');
+//         console.log('click')
+//     }
+//     else{
+//         $('.toggle_menu_fon').addClass('none');
+//         $('.toggle_but').removeClass('close');
+//         console.log('click2')
+//     }
+    
+// });
--- main.php
+++ main.php
@@ -7,27 +7,30 @@
     <link rel="stylesheet" href="css/main_style.css">
     <script src="https://code.jquery.com/jquery-3.6.0.js"
         integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="crossorigin="anonymous"></script>
-        <script src="js/main.js"></script>
-        <title>Главная: Системные администраторы</title>
+        
+    <title>Главная: Системные администраторы</title>
 </head>
 <body>
     <!-- HEADER -->
-    <nav>   
+    <nav>  
+         
         <!-- NAVIGATION PANEL      -->
         <div class="navigation">
             <div class="logo_wrap" onselectstart="return false" onmousedown="return false">
                 <img src="icon/logo.png" alt="">
             </div>
 
-            <!-- TOOGLE BAR -->
+            <!-- TOOGLE BUTTON -->
             <div class="toggle_bar">
-
                 <div class="toggle_but border button">
                     <span></span>
                 </div>
+            </div>
 
-                <div class="toggle_menu">
-                    <!-- <span class="button border main">
+            <!-- TOOGLE MENU -->
+            <div class="toggle_menu_fon">
+                <div class="toggle_menu border">
+                     <span class="button border main">
                         <img src="icon/Untitled.svg" alt="alt">
                         <p class="font_18">Системные администраторы</p>
                     </span>
@@ -48,9 +51,8 @@
                     <span class="button border door">                    
                         <p class="font_18">Выйти</p>
                         <img src="icon/exitdoor_87195.svg" alt="alt">
-                    </span> -->
-                </div>
-              
+                    </span>
+                </div>                   
             </div>
 
             <!-- BUTTONS -->
@@ -98,6 +100,6 @@
         </div>  
     </div>
 
-    
+    <script src="js/main.js"></script>
 </body>
 </html>
