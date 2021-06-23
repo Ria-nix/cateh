da7b75c86952527f645a7a238faf63ac1f3d41b7
Ria-nix <60116632+Ria-nix@users.noreply.github.com>
2021-06-23 20:04:09 +0200

    Add files via upload

---
+++ navigation.js
@@ -0,0 +1,35 @@
+for(let elem of $('.admin')){
+    elem.addEventListener('click',function(){
+        window.location.href = 'index.html';
+    })
+}
+
+for(let elem of $('.admin_plus')){
+    elem.addEventListener('click',function(){
+        window.location.href = 'plus.html';
+    })
+}
+
+for(let elem of $('.organization')){
+    elem.addEventListener('click',function(){
+        window.location.href = 'plus.html';
+    })
+}
+
+for(let elem of $('.organization_plus')){
+    elem.addEventListener('click',function(){
+        window.location.href = 'plus.html';
+    })
+}
+
+for(let elem of $('.settings')){
+    elem.addEventListener('click',function(){
+        window.location.href = 'plus.html';
+    })
+}
+
+for(let elem of $('.exit')){
+    elem.addEventListener('click',function(){
+        window.location.href = 'plus.html';
+    })
+}
