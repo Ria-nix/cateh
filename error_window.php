bb5e4efc7c2c41ab4f01e64578d0accc3c506ffd
Ria-nix <podkovadasha@gmail.com>
2021-06-25 12:49:07 +0200

    .

---
+++ error_window.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/modal_window.css">
+    <title>ошибка</title>
+</head>
+<body>
+    <div class="modal_window">
+        <div class="window_wrap border">
+            <img src="icon/question.svg" alt="question">
+            <p class="font_18">Вы точно хотите добавить нового системного администратора?</p>
+            <button class="button border font_18">Закрыть</button>
+        </div>
+    </div>
+</body>
+</html>
--- question_window.php
+++ question_window.php
@@ -4,12 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="modal_window.css">
+    <link rel="stylesheet" href="css/modal_window.css">
     <title>question</title>
 </head>
 <body>
     <div class="question_window">
-        
+        <div class="window_wrap border">
+            <img src="icon/question.svg" alt="question">
+            <p class="font_18">Вы точно хотите добавить нового системного администратора?</p>
+            <div class="buttons">
+                <button class="button border font_18">Отменить</button>
+                <button class="button border font_18">Добавить</button>
+            </div>
+        </div>
     </div>
 </body>
 </html>
