45179656445a8d85e37054534e539fc5a638916c
Ria-nix <podkovadasha@gmail.com>
2021-06-25 12:07:38 +0200

    modal windows

---
+++ question_window.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="modal_window.css">
+    <title>question</title>
+</head>
+<body>
+    <div class="question_window">
+        
+    </div>
+</body>
+</html>
