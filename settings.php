1ff511119c0cef9c0de022b7dfa3d4a1bf2b3efd
Ria-nix <podkovadasha@gmail.com>
2021-06-25 09:30:58 +0200

    menu

--- settings.php
+++ settings.php
@@ -11,76 +11,9 @@
     <title>Настройки</title>
 </head>
 <body>
-    <!-- HEADER -->
-    <nav>           
-
-        <!-- NAVIGATION PANEL      -->
-        <div class="navigation">            
-            <img src="icon/logo.png" class="logo" alt="logo">
-
-            <!-- TOOGLE BUTTON -->
-            <div class="toggle_bar">
-                <div class="toggle_but border button">
-                    <span></span>
-                </div>
-            </div>
-
-            <!-- TOOGLE MENU -->
-            <div class="toggle_menu_fon none">
-                <div class="toggle_menu border">
-                     <span class="button border main admin">
-                        <img src="icon/Untitled.svg" alt="alt">
-                        <p class="font_18">Системные администраторы</p>
-                    </span>
-                    <span class="button border plus admin_plus">
-                        <img src="icon/plus-solid.svg" alt="alt">
-                        <p class="font_18 icon_none none">Добавить нового сис. админа</p>
-                    </span>
-                    <span class="button border main organization">
-                        <img src="icon/build.svg" alt="alt">
-                        <p class="font_18">Организации</p>
-                    </span>
-                    <span class="button border plus organization_plus">
-                        <img src="icon/plus-solid.svg" alt="alt">
-                        <p class="font_18 icon_none none">Добавить новую организацию</p>
-                    </span>
-                    <span class="button border settings">
-                        <img src="icon/settings.svg" alt="alt">
-                        <p class="font_18 icon_none none">Настройки</p>
-                    </span>
-                    <span class="button border door exit">                    
-                        <p class="font_18">Выйти</p>
-                        <img src="icon/exitdoor_87195.svg" alt="alt">
-                    </span>
-                </div>                   
-            </div>
-
-            <!-- BUTTONS -->
-            <div class="panel_buttons">
-                <span class="button border main admin">
-                    <img src="icon/Untitled.svg" alt="alt">
-                    <p class="font_18">Системные администраторы</p>
-                </span>
-                <span class="button border plus admin_plus">
-                    <img src="icon/plus-solid.svg" alt="alt">
-                </span>
-                <span class="button border main organization">
-                    <img src="icon/build.svg" alt="alt">
-                    <p class="font_18">Организации</p>
-                </span>
-                <span class="button border plus organization_plus">
-                    <img src="icon/plus-solid.svg" alt="alt">
-                </span>
-                <span class="button border settings">
-                    <img src="icon/settings.svg" alt="alt">
-                    <p class="font_18"></p>
-                </span>
-                <span class="button border door exit">                    
-                    <p class="font_18">Выйти</p>
-                    <img src="icon/exitdoor_87195.svg" alt="alt">
-                </span>
-            </div>
-        </div>
+    <?php 
+        include_once('main_menu.php');
+    ?>
 
     </nav>
     <div class="content_wrap">
