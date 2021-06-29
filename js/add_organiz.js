a9ab8c74544028eb29a36ab771518263c79fb48f
Ria-nix <podkovadasha@gmail.com>
2021-06-29 16:18:01 +0200

    /

--- js/add_organiz.js
+++ js/add_organiz.js
@@ -1,32 +1,29 @@
+let summ = 1, inputs;
 $('#add_adress').click(function(){
     if($('#adress').val() == ''){ 
         $('#adress').addClass('red_auto');
-        $('#adress').attr('placeholder','Введите первый адрес для добавления еще одного ');
-        console.log('stop'); 
-
+        $('#adress').attr('placeholder','Введите адрес для добавления еще одного ');         
     }
     else{
-        if($('.field_adress').last('.text_adress').val() == ''){
-            $('#adress').addClass('red_auto');
-            $('#adress').attr('placeholder','Введите первый адрес для добавления еще одного ');
-        }
+        $('#adress').removeClass('red_auto'); 
+        if(summ == 23){ inputs = ''; }
         else{
-            let inputs = '<div class="cell"><input type="text" class="text_adress"><img src="icon/close.svg" alt="close"></div>';
-            $('.fields_adress').append(inputs);        
-            console.log('click'); 
-            $('#adress').removeClass('red_auto');           
-            
+            inputs = '<div class="cell"><input type="text" class="text_adress"><img src="icon/close.svg" alt="close"></div>';
+            $('.fields_adress').append(inputs);    
+            summ++; 
+
+            for(var elem of $('.cell').children('img')){ }
+            elem.addEventListener('click', function(){ elem.parentElement.remove();  summ--});
         }
-        
-        for(var elem of $('.cell').children('img')){ }
-            elem.addEventListener('click', function(){           
-                elem.parentElement.remove();            
-            })
-       
 
-
-
-
+        // if($('.field_adress').last('.text_adress').val() == ''){
+        //     $('#adress').addClass('red_auto');
+        //     $('#adress').attr('placeholder','Введите адрес для добавления еще одного ');
+        // }
+        // else{
+                    
+            
+        // }
         
     }
 })
