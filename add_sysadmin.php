857cd7f95cb07c95a1592922cdb3fd0d4110c00d
Ria-nix <podkovadasha@gmail.com>
2021-07-13 11:16:56 +0200

    //.

--- add_sysadmin.php
+++ add_sysadmin.php
@@ -15,10 +15,9 @@
 
     <!-- CONTENT -->
     <div class="content_wrap">
-
         <p class="font_24 bold">Добавление системного администратора</p>
         <div class="content_set">
-
+            
             <div class="main_fields">
                 <label for="login">Логин</label>
                 <input type="text" id="login" class="add_info">
@@ -46,67 +45,46 @@
 
             <div class="field_choice main_fields">
                 <label for="address" class="text_width">Добавьте компетенцию</label>
-               <!-- <input type="text" id="address" class="text_address add_info"> -->
-               <input type="text" class="select border spacing" list="competence" placeholder="Введите компетенцию">
-               <datalist name="select_name" id="competence">
-                   <option value="vanilla" selected disaibled hidden></option>
-                <!-- <span class="polygon">
-                    <img src="icon/Polygon.svg" alt="alt">
-                </span> -->
-                    <option class="item_options" value="new"></option>
-                    <option class="item_options" value="chocolate"></option>
-                    <option class="item_options" value="mint"></option>
-                    <option class="item_options" value="latte"></option>
-                    <option class="item_options" value="spain"></option>
-                    <option class="item_options" value="rock"></option>
-                    <option class="item_options" value="letter"></option>
-                    <option class="item_options" value="nroll"></option>
-                    <option value="Добавить" id="add_competence">
-                        <img src="icon/plus-solid.svg" alt="alt">
-                    </option>
-               </datalist>
-                
-                <!-- <div class="options border"> -->
-                    <!-- <option class="item_options">spain spain spain spain spain spain spain spain spain</option>
-                    <option class="item_options">spain</option>
-                    <option class="item_options">spain</option>
-                    <option class="item_options">spain</option>
-                    <option class="item_options">spain</option>
-                    <option class="item_options">spain</option>
-                    <option class="item_options">spain</option>
-                    <option class="item_options">spain</option>
-                    <option value="add_competence" id="add_competence">
-                        <img src="icon/plus-solid.svg" alt="alt">Добавить
-                    </option> -->
-                <!-- </div> -->
-                
-                    
-            </div>
 
-            <div class="list_competence border">
-              
+                <div class="searchable border">
+                    <input type="text" class=" border font_18" id="list_competence" placeholder="Введите компетенцию">
+                    <span class="polygon">
+                        <img src="icon/Polygon.svg" alt="alt">
+                    </span>
+                    <ul class="border font_18 none" id="list">
+                        <li>Algeria</li>
+                        <li>Bulgaria</li>
+                        <li>Canada</li>
+                        <li>Egypt</li>
+                        <li>Fiji</li>
+                        <li>India</li>
+                        <li>Japan</li>
+                        <li>Iran (Islamic Republic of)</li>
+                        <li>Lao People's Democratic Republic</li>
+                        <li>Micronesia (Federated States of)</li>
+                        <li>Nicaragua</li>
+                        <li>Senegal</li>
+                        <li>Tajikistan</li>
+                        <li>Yemen</li>
+                    </ul>
+                </div>
             </div>
+            <div class="list_competence border"></div>
 
             <div class="img_organization">
-
                 <div class="image border">
                     <img class="logo border" src="icon/michael.jpg" alt="logo">
                 </div>                
-
                 <div class="img_buttons">
                 <input type="file" accept="img/*" id="input_file">
                     <label class="button_gray border font_16" for="input_file" id="download_file">Загрузить</label>
                     <button class="button_gray border font_16" id="delete_img">Удалить</button>
                 </div>
-
             </div>
-
             <div class="save">
                 <button class="button border font_16" id="check_button">Добавить</button>
             </div>
-
         </div>
-
     </div>
 
     <script src="js/modal_windows.js"></script>    
--- js/add_sysadmin.js
+++ js/add_sysadmin.js
@@ -1,4 +1,68 @@
-$(document).ready(function () {
+$(document).ready(function(){
+    let bool = false;
+    $('#list_competence').on('click', function(){
+        console.log('re')
+       if(!bool){ 
+           $('#list').removeClass('none')
+           bool == true;
+        }
+       else{
+            $('#list').addClass('none');
+            bool = false;
+       } 
+
+    })
+//     function filterFunction(that) {
+//         let container, input, filter, li, input_val;
+//         container = $(that).closest(".searchable");
+//         input_val = container.find("input").val().toUpperCase();
+    
+//         if (["ArrowDown", "ArrowUp", "Enter"].indexOf(event.key) != -1) {
+//             keyControl(event, container)
+//         } else {
+//             li = container.find("ul li");
+//             li.each(function (i, obj) {
+//                 if ($(this).text().toUpperCase().indexOf(input_val) > -1) {
+//                     $(this).show();
+//                 } else {
+//                     $(this).hide();
+//                 }
+//             });
+    
+//             container.find("ul li").removeClass("selected");
+//             setTimeout(function () {
+//                 container.find("ul li:visible").first().addClass("selected");
+//             }, 100)
+//         }
+//     }
+    
    
+//     function onSelect(val) {
+//         alert(val)
+//     }
+    
+//     $(".searchable input").focus(function () {
+//         $(this).closest(".searchable").find("ul").show();
+//         $(this).closest(".searchable").find("ul li").show();
+//     });
+//     $(".searchable input").blur(function () {
+//         let that = this;
+//         setTimeout(function () {
+//             $(that).closest(".searchable").find("ul").hide();
+//         }, 300);
+//     });
+    
+//     $(document).on('click', '.searchable ul li', function () {
+//         $(this).closest(".searchable").find("input").val($(this).text()).blur();
+//         onSelect($(this).text())
+//     });
+    
+//     $(".searchable ul li").hover(function () {
+//         $(this).closest(".searchable").find("ul li.selected").removeClass("selected");
+//         $(this).addClass("selected");
+//     });
+
+
+
 });
 
