194fc7014bc1866cecb1e9eea05c5e5ce81167cd
Ria-nix <podkovadasha@gmail.com>
2021-07-29 09:07:43 +0200

    /.././..///

---
+++ modal windows/password_window.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/modal_window.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="modal_window error none">
+        <div class="window_wrap border">
+            <img src="icon/x-mark.svg" alt="question">
+            <p class="font_18"> <strong>8 999 558 QJU 15D DHJ</strong>
+            <br>Запишите новый пароль организации,
+                так как в последующем Вы не сможете просмотреть
+                его в профиле данной организации </p>
+            <button class="button border font_18 close">Закрыть</button>
+        </div>
+    </div>
+</body>
+</html>
+
+
