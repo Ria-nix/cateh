56080966f700977202fb079bbcfa7125eb1261ab
Ria-nix <podkovadasha@gmail.com>
2021-08-19 13:12:07 +0200

    ./././././//

--- profile_sysadmin.php
+++ profile_sysadmin.php
@@ -30,15 +30,15 @@ include('head_code.php'); ?>
                     <p class="spacing font_22">Михаил Куприн</p>
                 </span>
                 <span class="text_info count">
-                    <p class="bold">Выполнено за месяц:</p>
+                    <p>Выполнено за месяц:</p>
                     <p class="opacity font_18">50</p>
                 </span>
                 <span class="text_info count">
-                    <p class="bold">Выполнено за все время:</p>
+                    <p>Выполнено за все время:</p>
                     <p class="opacity font_18">50</p>
                 </span>
                 <span class="text_info count">
-                    <p class="bold">Текущие заказы:</p>
+                    <p>Текущие заказы:</p>
                     <p class="opacity font_18">50</p>
                 </span>
 
@@ -49,11 +49,11 @@ include('head_code.php'); ?>
                         <p class="bold">Выполнено за месяц</p>
                     </span>
                     <span class="text_info mobile_item">
-                        <p class="opacity font_18">50</p>
+                        <p class="font_18">50</p>
                         <p class="bold">Выполнено за все время</p>
                     </span>
                     <span class="text_info count">
-                        <p class="opacity font_18">50</p>
+                        <p class="font_18">50</p>
                         <p class="bold">Текущие заказы</p>
                     </span>
 
@@ -66,7 +66,7 @@ include('head_code.php'); ?>
             </div>
 
             <div class="number">
-                <p class="spacing">Номер телефона:<br><b> +7(***)*** - 00 - 00</b> </p> 
+                <p class="spacing">Номер телефона:<br><span class="b"> +7(***)*** - 00 - 00</span> </p> 
             </div>
             <div class="rating">
                 <div class="item_rating" id="red"></div>
@@ -77,24 +77,44 @@ include('head_code.php'); ?>
             </div>
         </div>
 
-        <div class="comtence_list">
+        <div class="competence_list">
             <p class="name">Компетенция:</p>
-            <div class="competences"></div>
+            <div class=" border competences">
+            <span class="border cell competence_item register">something  something something something something something something something something something something something something something something something something somethingsomething something<img src="icon/close.svg" alt="close"></span>
+            <span class="border cell competence_item register">something new<img src="icon/close.svg" alt="close"></span>
+            <span class="border cell competence_item register">something new<img src="icon/close.svg" alt="close"></span>
+            <span class="border cell competence_item register">something new<img src="icon/close.svg" alt="close"></span>
+            <span class="border cell competence_item register">something new<img src="icon/close.svg" alt="close"></span>
+            <span class="border cell competence_item register">something new<img src="icon/close.svg" alt="close"></span>
+            <span class="border cell competence_item register">something new<img src="icon/close.svg" alt="close"></span>
+            <span class="border cell competence_item register">something new<img src="icon/close.svg" alt="close"></span>
+            <span class="border cell competence_item register">something new<img src="icon/close.svg" alt="close"></span>
+            <span class="border cell competence_item register">something new<img src="icon/close.svg" alt="close"></span>
+            <span class="border cell competence_item register">something new<img src="icon/close.svg" alt="close"></span>
+            <span class="border cell competence_item register">something new<img src="icon/close.svg" alt="close"></span>
+            <span class="border cell competence_item register">something new<img src="icon/close.svg" alt="close"></span>
+            <span class="border cell competence_item register">something new<img src="icon/close.svg" alt="close"></span>
+            <span class="border cell competence_item register">something new<img src="icon/close.svg" alt="close"></span>
+            <span class="border cell competence_item register">something new<img src="icon/close.svg" alt="close"></span>
+            <span class="border cell competence_item register">something new<img src="icon/close.svg" alt="close"></span>
+            <span class="border cell competence_item register">something new<img src="icon/close.svg" alt="close"></span>
+
+            </div>
         </div>
     </div>
     <!-- CONTENT -->
     <div class="content_wrap font_16">
         <!-- HEAD TABLE -->
-        <!-- <div class="head_table">
+        <div class="head_table">
             <div class="head_name spacing_special">
                 <p>ФИО <span class="ask_des_arrow"><img src="icon/up_arrow.svg" alt="arrow" id="arrow"></span></p>
                 <p>Выполнено за месяц</p>
                 <p>Текущих заказов </p>
                 <p>Статус</p>
             </div>
-        </div> -->
+        </div>
         <!-- TABLE -->
-        <!-- <div class="table_sysadmin" id="table">
+        <div class="table_sysadmin" id="table">
             <div class="item_table"><div class="item_info border"><p class="main_fio">milaninn</p><div class="mobile_version"><div class="mobile_title"><p>Выполнено за месяц</p><p>Текущих заказов </p><p class="role">Роль</p></div><div class="mobile_info"><p class="complete_order">reorewkjlkfs</p><p class="current_order">sdkjflskjfkksjkd</p><p class="role">newksjfksdf</p></div></div><p class="mobile_none complete_order">kjkjsdfjfjkkljdsfk</p><p class="mobile_none current_order">jdslfjsdkjfskd</p><p class="mobile_none role"><skdf;lskdfksdfksdlfkls/p></div><button class="click button border font_16">Просмотреть</button></div>
             <div class="item_table"><div class="item_info border"><p class="main_fio">milaninn</p><div class="mobile_version"><div class="mobile_title"><p>Выполнено за месяц</p><p>Текущих заказов </p><p class="role">Роль</p></div><div class="mobile_info"><p class="complete_order">reorewkjlkfs</p><p class="current_order">sdkjflskjfkksjkd</p><p class="role">newksjfksdf</p></div></div><p class="mobile_none complete_order">kjkjsdfjfjkkljdsfk</p><p class="mobile_none current_order">jdslfjsdkjfskd</p><p class="mobile_none role">skdf;lskdfksdfksdlfkls</p></div><button class="click button border font_16">Просмотреть</button></div>
             <div class="item_table"><div class="item_info border"><p class="main_fio">milaninn</p><div class="mobile_version"><div class="mobile_title"><p>Выполнено за месяц</p><p>Текущих заказов </p><p class="role">Роль</p></div><div class="mobile_info"><p class="complete_order">reorewkjlkfs</p><p class="current_order">sdkjflskjfkksjkd</p><p class="role">newksjfksdf</p></div></div><p class="mobile_none complete_order">kjkjsdfjfjkkljdsfk</p><p class="mobile_none current_order">jdslfjsdkjfskd</p><p class="mobile_none role"><skdf;lskdfksdfksdlfkls/p></div><button class="click button border font_16">Просмотреть</button></div>
@@ -116,7 +136,7 @@ include('head_code.php'); ?>
             <div class="item_table"><div class="item_info border"><p class="main_fio">milaninn</p><div class="mobile_version"><div class="mobile_title"><p>Выполнено за месяц</p><p>Текущих заказов </p><p class="role">Роль</p></div><div class="mobile_info"><p class="complete_order">reorewkjlkfs</p><p class="current_order">sdkjflskjfkksjkd</p><p class="role">newksjfksdf</p></div></div><p class="mobile_none complete_order">kjkjsdfjfjkkljdsfk</p><p class="mobile_none current_order">jdslfjsdkjfskd</p><p class="mobile_none role"><skdf;lskdfksdfksdlfkls/p></div><button class="click button border font_16">Просмотреть</button></div>
             <div class="item_table"><div class="item_info border"><p class="main_fio">milaninn</p><div class="mobile_version"><div class="mobile_title"><p>Выполнено за месяц</p><p>Текущих заказов </p><p class="role">Роль</p></div><div class="mobile_info"><p class="complete_order">reorewkjlkfs</p><p class="current_order">sdkjflskjfkksjkd</p><p class="role">newksjfksdf</p></div></div><p class="mobile_none complete_order">kjkjsdfjfjkkljdsfk</p><p class="mobile_none current_order">jdslfjsdkjfskd</p><p class="mobile_none role">skdf;lskdfksdfksdlfkls</p></div><button class="click button border font_16">Просмотреть</button></div>
             <div class="item_table"><div class="item_info border"><p class="main_fio">milaninn</p><div class="mobile_version"><div class="mobile_title"><p>Выполнено за месяц</p><p>Текущих заказов </p><p class="role">Роль</p></div><div class="mobile_info"><p class="complete_order">reorewkjlkfs</p><p class="current_order">sdkjflskjfkksjkd</p><p class="role">newksjfksdf</p></div></div><p class="mobile_none complete_order">kjkjsdfjfjkkljdsfk</p><p class="mobile_none current_order">jdslfjsdkjfskd</p><p class="mobile_none role"><skdf;lskdfksdfksdlfkls/p></div><button class="click button border font_16">Просмотреть</button></div>
-        </div>   -->
+        </div>  
 
     </div>
 
